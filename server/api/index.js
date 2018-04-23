4a70fce4fd9728c20068121b06376f55ca4353d9
Doris Cheng <dorcheng@umich.edu>
2018-04-23 09:55:54 -0400

    Deploy to heroku

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ const api = require('express').Router();
 api.use('/questions', require('./routes/question'));
 
 api.use((req, res, next) => {
-  res.status(404).send('Not found');
+  res.status(404).send('Content not found');
 });
 
 module.exports = api;

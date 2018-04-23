1f481a3869c933aecabc6183427e86c8af7c7956
Doris Cheng <dorcheng@umich.edu>
2018-04-23 09:53:18 -0400

    Deploy to Heroku

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api', require('./api'));
 
-app.get('/', (req, res) => {
+app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
